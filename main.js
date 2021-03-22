a2c77ee9c03b8a79bdb4e39a7c93f9563b6a606d
seanrho94 <59059144+seanrho94@users.noreply.github.com>
2021-03-22 20:47:35 +1100

    JS: changed scroll to section function for better browser compatibility

--- main.js
+++ main.js
@@ -3,27 +3,42 @@ const menu = document.querySelector(".navbar__menu");
 const toggleBtn = document.querySelector(".navbar__toggle-btn");
 const cancelBtn = document.querySelector(".navbar__cancel-btn");
 
-window.onscroll = ()=>{
-  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+//Change navbar color on scrolling.
+window.onscroll = (()=>{
+  if (this.scrollY > 20) {
+    navbar.classList.add("sticky")
+  }else {
+    navbar.classList.remove("sticky")
+  }
+});
 
-toggleBtn.onclick = ()=>{
+//Handle hamburger menu in mobile view.
+toggleBtn.onclick = (()=>{
   menu.classList.add("active");
   toggleBtn.classList.add("hide");
-}
+});
 
-cancelBtn.onclick = ()=>{
+//Handle cancel (to close navbar menu) button in mobile view.
+cancelBtn.onclick = (()=>{
   menu.classList.remove("active");
   toggleBtn.classList.remove("hide");
-}
+});
 
-//Handle scrolling when tapping on the navbar menu
+//Close responsive navbar menu after navigating to the section.
 menu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link
   if(link == null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  menu.classList.remove("active");
+  toggleBtn.classList.remove("hide");
 });
+
+//Handle scrolling when tapping on the navbar menu (https://github.com/cferdinandi/smooth-scroll)
+const scroll = new SmoothScroll('navbar, .navbar__menu a[href*="#"]', {
+  header: '[data-scroll-header]',
+  speed: 800
+});
+
+
